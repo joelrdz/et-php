14d3ec90f0eb5be20ab5c73482de922ac20df96b
Joel Rdz <joelrdzh@gmail.com>
2023-05-24 19:41:47 -0600

    array_filter for better lists experience

--- index.php
+++ index.php
@@ -14,19 +14,23 @@
     ],
   ];
 
+  $completedTasks = array_filter($tasks, function ($task) {
+    return $task['completed'];
+  });
+
+  $pendingTasks = array_filter($tasks, function ($task) {
+    return !$task['completed'];
+  });
+
   echo '<h1>My tasks</h1>';
 
   echo '<h2>Completed</h2>';
-  foreach ($tasks as $task) {
-    if ($task['completed']) {
-      echo $task['title'] . ($task['completed'] ? '✅' : '❌') . '<br>';
-    }
+  foreach ($completedTasks as $task) {
+    echo $task['title'] . ' ✅<br>';
   }
 
   echo '<h2>Pending</h2>';
-  foreach ($tasks as $task) {
-    if (!$task['completed']) {
-      echo $task['title'] . ($task['completed'] ? '✅' : '❌') . '<br>';
-    }
+  foreach ($pendingTasks as $task) {
+    echo $task['title'] . ' ❌<br>';
   }
 ?>
