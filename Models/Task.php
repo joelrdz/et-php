648ee680d70fdf786dc778a542f9f3236d18c493
Joel Rdz <joelrdzh@gmail.com>
2023-06-07 12:18:57 -0600

    Constructor Property Promotion
    - Task class simplification

--- Models/Task.php
+++ Models/Task.php
@@ -1,13 +1,10 @@
 <?php
 
 class Task {
-  public $title;
-  public $completed;
-
-  public function __construct($title, $completed = false) {
-    $this->title = $title;
-    $this->completed = $completed;
-  }
+  public function __construct(
+    public $title,
+    public $completed = false
+  ) {}
 
   public function complete() {
     $this->completed = true;
