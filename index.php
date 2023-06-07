088aceeaf5990ebefcd8764e3440655d6d3962ec
Joel Rdz <joelrdzh@gmail.com>
2023-06-07 11:22:32 -0600

    php class constructor
    - to initialize object with a title and a completed status

--- index.php
+++ index.php
@@ -7,13 +7,18 @@ class Task {
   public $title;
   public $completed = false;
 
+  public function __construct($title, $completed) {
+    $this->title = $title;
+    $this->completed = $completed;
+  }
+
   public function complete() {
     $this->completed = true;
   }
 }
 
-$task = new Task;
-$task->complete();
+$task = new Task('Study PHP', true);
+// $task->complete();
 dd($task);
 
 $tasks = [
