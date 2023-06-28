2a4e08f78e7a5336995eef4788153edab4ddb4d7
Joel Rdz <joelrdzh@gmail.com>
2023-06-28 14:44:42 -0600

    adding Exam model with repeated methods from Task

--- Models/Task.php
+++ Models/Task.php
@@ -22,5 +22,26 @@ class Task {
   }
 }
 
+class Exam {
+  public function __construct(
+    public $title,
+    public $completed = false
+  ) {}
+
+  public function buildString() {
+    return "Title: {$this->title}\n" .
+    "Completed: " . ($this->completed ? 'Si' : 'No');
+  }
+
+  public function save($name) {
+    $file = fopen($name, 'w');
+    fwrite($file, $this->buildString());
+    fclose($file);
+  }
+}
+
 $task = new Task('Buy food', true);
 $task->save('task-1.txt');
+
+$exam = new Exam('PHP Exam', true);
+$exam->save('exam-1.txt');
