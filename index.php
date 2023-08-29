35999dade2359f469e199ba1589d6b5d6c32de16
Joel Rdz <joelrdzh@gmail.com>
2023-08-28 21:23:49 -0600

    showing data from database

--- index.php
+++ index.php
@@ -15,17 +15,17 @@ $query = $pdo->prepare('select * from tasks');
 
 $query->execute();
 
-dd($query->fetchAll(PDO::FETCH_OBJ));
+$tasks = $query->fetchAll(PDO::FETCH_OBJ);
 
-$tasks = [
-  new Task(completed: true, title: 'Study PHP'),
-  new Task('Buy food'),
-  new Task('Workout', true),
-];
+// $tasks = [
+//   new Task(completed: true, title: 'Study PHP'),
+//   new Task('Buy food'),
+//   new Task('Workout', true),
+// ];
 
-$tasks[0]->setColor(ColorsEnum::BLUE->value);
-$tasks[1]->setColor(ColorsEnum::GREEN->value);
-$tasks[2]->setColor(ColorsEnum::RED->value);
+// $tasks[0]->setColor(ColorsEnum::BLUE->value);
+// $tasks[1]->setColor(ColorsEnum::GREEN->value);
+// $tasks[2]->setColor(ColorsEnum::RED->value);
 
 $completedTasks = array_filter($tasks, function ($task) {
   return $task->completed;
