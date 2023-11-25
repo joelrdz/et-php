b397bc1221e7624ab00b3bc611358fca9a463f96
Joel Rdz <joelrdzh@gmail.com>
2023-11-25 16:25:41 -0600

    Dependency Injection
    - setting and getting app config from App dependency injector using static methods

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -1,11 +1,15 @@
 <?php
 require 'database/Connection.php';
 require 'database/QueryBuilder.php';
+require 'Core/App.php';
 require 'Core/Request.php';
 require 'Core/Router.php';
 require 'Models/Task.php';
 require 'functions.php';
-$config = require 'config.php';
+
+App::set('config', require 'config.php');
+
+$config = App::get('config');
 
 $pdo = Connection::start($config['database']);
 
