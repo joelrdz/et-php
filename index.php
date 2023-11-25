7c743a733b8496645c16c70e2177c4b3968bf706
Joel Rdz <joelrdzh@gmail.com>
2023-11-24 21:43:49 -0600

    separating routes in its own file

--- index.php
+++ index.php
@@ -4,15 +4,7 @@ $query = require 'Core/bootstrap.php';
 
 $url = trim($_SERVER['REQUEST_URI'], '/');
 
-$routes = [
-  '' => 'Controllers/index.php',
-  'about' => 'Controllers/about.php',
-  'contact' => 'Controllers/contact.php',
-  'services' => 'Controllers/services.php',
-  'task/create' => 'Controllers/create-task.php',
-  'task/toggle' => 'Controllers/toggle-task.php',
-  'task/delete' => 'Controllers/delete-task.php',
-];
+$routes = require('routes.php');
 
 $router = new Router;
 $router->register($routes);
---
+++ routes.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  '' => 'Controllers/index.php',
+  'about' => 'Controllers/about.php',
+  'contact' => 'Controllers/contact.php',
+  'services' => 'Controllers/services.php',
+  'task/create' => 'Controllers/create-task.php',
+  'task/toggle' => 'Controllers/toggle-task.php',
+  'task/delete' => 'Controllers/delete-task.php',
+];
